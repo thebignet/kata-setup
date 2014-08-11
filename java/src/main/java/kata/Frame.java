bf16ca1aa70de37d5bd9685d4da7ce2b47fe211b
Johan Martinsson <martinsson.johan@gmail.com>
2014-08-12 00:27:22 +0200

    adding design comments

--- java/src/main/java/kata/Frame.java
+++ java/src/main/java/kata/Frame.java
@@ -2,11 +2,15 @@ package kata;
 
 public interface Frame {
 
+    // public interface
     int score();
     
+    // internal business, only frames should know about it
     int nextTwoBalls();
     int getFirst();
 
+    // must be used to finalize construction, 
+    // introduces implicit temporary coupling
     void setNext(Frame nextFrame);
     
     
