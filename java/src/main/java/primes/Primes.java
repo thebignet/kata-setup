7b22cfea3d0f8e93a2aaa725e730483ad9d28e99
Johan Martinsson <martinsson.johan@gmail.com>
2014-12-03 00:10:21 +0100

    introducing division

--- java/src/main/java/primes/Primes.java
+++ java/src/main/java/primes/Primes.java
@@ -11,17 +11,16 @@ public class Primes {
     public List<Integer> factorsOf(int i) {
 	List<Integer> factors = new ArrayList<Integer>();
 	if (i == 2) {
-	    factors.add(2);
+	    factors.add(2 / 1);
 	}
 	if (i == 4) {
-	    factors.add(2);
-	    factors.add(2);
+	    factors.add(2 / 1);
+	    factors.add(4 / 2);
 	}
 	if (i == 8) {
-	    factors.add(2);
-	    factors.add(2);
-	    factors.add(2);
-	    
+	    factors.add(2 / 1);
+	    factors.add(4/ 2);
+	    factors.add(8/ 4);
 	}
 	if (i == 3) factors.add(3);
 	if (i == 5) factors.add(5);
