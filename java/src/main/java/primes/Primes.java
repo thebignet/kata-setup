69354e87d51ee417d57a9aec3851fead9fe69768
Johan Martinsson <martinsson.johan@gmail.com>
2014-12-03 00:29:44 +0100

    introduced 4

--- java/src/main/java/primes/Primes.java
+++ java/src/main/java/primes/Primes.java
@@ -18,6 +18,10 @@ public class Primes {
 	    factors.add(3);
 	    i = i/3;
 	}
+	while (i / 4 >= 1 && i % 4 == 0) {
+	    factors.add(4);
+	    i = i/4;
+	}
 	while (i / 5 >= 1 && i % 5 == 0) {
 	    factors.add(5);
 	    i = i/5;
