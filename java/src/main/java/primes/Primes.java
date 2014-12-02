3a0309f1719dec0722bdd7535205cbc8801262cf
Johan Martinsson <martinsson.johan@gmail.com>
2014-12-03 00:07:08 +0100

    introduced mutable list

--- java/src/main/java/primes/Primes.java
+++ java/src/main/java/primes/Primes.java
@@ -2,18 +2,20 @@ package primes;
 
 import static java.util.Arrays.asList;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Primes {
 
     public List<Integer> factorsOf(int i) {
-	if (i == 2) return asList(2);
+	List<Integer> factors = new ArrayList<Integer>();
+	if (i == 2) factors.add(2);
 	if (i == 4) return asList(2, 2);
 	if (i == 8) return asList(2, 2, 2);
 	if (i == 3) return asList(3);
 	if (i == 5) return asList(5);
-	return Arrays.asList();
+	return factors;
     }
 
 }
