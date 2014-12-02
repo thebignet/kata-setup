808459c77b960f85125fd3900d1200256f38d8a0
Johan Martinsson <martinsson.johan@gmail.com>
2014-12-03 00:38:57 +0100

    renames extract method

--- java/src/main/java/primes/Primes.java
+++ java/src/main/java/primes/Primes.java
@@ -8,16 +8,19 @@ import java.util.List;
 
 public class Primes {
 
-    public List<Integer> factorsOf(int i) {
+    public List<Integer> factorsOf(int number) {
 	List<Integer> factors = new ArrayList<Integer>();
-	for (int potentialPrime = 2; potentialPrime <=i; potentialPrime++) {
-	    while (i / potentialPrime >= 1 && i % potentialPrime == 0) {
+	for (int potentialPrime = 2; potentialPrime <=number; potentialPrime++) {
+	    while (number >= potentialPrime && isDividableBy(number, potentialPrime)) {
 		factors.add(potentialPrime);
-		i = i/potentialPrime;
+		number = number/potentialPrime;
 	    }
-	    
 	}
 	return factors;
+    }
+
+    protected boolean isDividableBy(int number, int potentialPrime) {
+	return number % potentialPrime == 0;
     }
 
 }
