c4c25dc4e3f1b6d0ed2a6de40cf1c7ef91f03b49
Johan Martinsson <martinsson.johan@gmail.com>
2014-12-02 23:53:16 +0100

    first test

---
+++ java/src/main/java/primes/Primes.java
@@ -0,0 +1,12 @@
+package primes;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Primes {
+
+    public List<Integer> factorsOf(int i) {
+	return Arrays.asList();
+    }
+
+}
---
+++ java/src/test/java/primes/PrimesTest.java
@@ -0,0 +1,19 @@
+package primes;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class PrimesTest {
+
+    @Test
+    public void test() {
+	Primes primes = new Primes();
+	List<Integer> factors = primes.factorsOf(1);
+	Assertions.assertThat(factors).isEmpty();
+    }
+
+}
