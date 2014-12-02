b64620fc520d1f465948f708921dcffede82418f
Johan Martinsson <martinsson.johan@gmail.com>
2014-12-03 00:20:47 +0100

    characterized / and %

--- java/src/test/java/primes/PrimesTest.java
+++ java/src/test/java/primes/PrimesTest.java
@@ -1,6 +1,7 @@
 package primes;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
@@ -28,6 +29,14 @@ public class PrimesTest {
     forMultiplesOfPrimes_thePrimeIsRepeated() throws Exception {
 	 assertThat(factorsOf(4)).containsExactly(2, 2);
 	 assertThat(factorsOf(8)).containsExactly(2, 2, 2);
+    }
+    
+    @Test public void 
+    testname() throws Exception {
+	assertEquals(4, 8 / 2); 
+	assertEquals(0, 8 % 2); 
+	assertEquals(3, 7 / 2); 
+	assertEquals(1, 7 % 2); 
     }
 
     private List<Integer> factorsOf(int number) {
