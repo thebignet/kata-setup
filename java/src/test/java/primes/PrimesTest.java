620a8cb67cd249d2f4a817cd1ec6794b3e70834f
Johan Martinsson <martinsson.johan@gmail.com>
2014-12-02 23:59:02 +0100

    refactored test

--- java/src/test/java/primes/PrimesTest.java
+++ java/src/test/java/primes/PrimesTest.java
@@ -1,22 +1,29 @@
 package primes;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 public class PrimesTest {
 
+    Primes primes = new Primes();
     // 0
     
     @Test
     public void forNumbersBelow2_noPrimes() {
-	Primes primes = new Primes();
-	Assertions.assertThat(primes.factorsOf(1)).isEmpty();
-    }
-    
-    @Test public void 
-    testname() throws Exception {
-	Primes primes = new Primes();
-	Assertions.assertThat(primes.factorsOf(2)).contains(2);
+	int i = 1;
+	Assertions.assertThat(factorsOf(i)).isEmpty();
     }
 
+    @Test public void 
+    for2_listOf2s() throws Exception {
+	int i = 2;
+	Assertions.assertThat(factorsOf(i)).contains(2);
+    }
+
+    private List<Integer> factorsOf(int number) {
+	return primes.factorsOf(number);
+    }
+    
 }
