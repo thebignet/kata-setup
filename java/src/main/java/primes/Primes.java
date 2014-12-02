cff87dca5766fcb909110c26112cf9948346565d
Johan Martinsson <martinsson.johan@gmail.com>
2014-12-03 00:08:52 +0100

    no immutable lists anymore

--- java/src/main/java/primes/Primes.java
+++ java/src/main/java/primes/Primes.java
@@ -10,11 +10,21 @@ public class Primes {
 
     public List<Integer> factorsOf(int i) {
 	List<Integer> factors = new ArrayList<Integer>();
-	if (i == 2) factors.add(2);
-	if (i == 4) return asList(2, 2);
-	if (i == 8) return asList(2, 2, 2);
-	if (i == 3) return asList(3);
-	if (i == 5) return asList(5);
+	if (i == 2) {
+	    factors.add(2);
+	}
+	if (i == 4) {
+	    factors.add(2);
+	    factors.add(2);
+	}
+	if (i == 8) {
+	    factors.add(2);
+	    factors.add(2);
+	    factors.add(2);
+	    
+	}
+	if (i == 3) factors.add(3);
+	if (i == 5) factors.add(5);
 	return factors;
     }
 
