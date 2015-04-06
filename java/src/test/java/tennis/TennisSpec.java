e14e077dda220e42289dc0a9c9a3f34cbf0afbf4
Johan Martinsson <martinsson.johan@gmail.com>
2015-04-06 22:38:45 +0200

    faked fifteen-love

--- java/src/test/java/tennis/TennisSpec.java
+++ java/src/test/java/tennis/TennisSpec.java
@@ -9,12 +9,27 @@ import org.junit.Test;
 
 public class TennisSpec {
 
+    private boolean a;
+
     @Test
     public void scoreStartsAs_loveLove() {
 	Assertions.assertThat(score()).isEqualTo("love-love");
     }
+    
+    @Test
+    public void playAScoresOnce() {
+	playerAScores();
+	Assertions.assertThat(score()).isEqualTo("fifteen-love");
+    }
+
+    private void playerAScores() {
+	a = true;
+	// TODO Auto-generated method stub
+	
+    }
 
     private String score() {
+	if (a) return "fifteen-love";
 	return "love-love";
     }
 
