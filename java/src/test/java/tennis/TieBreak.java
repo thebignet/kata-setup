c891626d82ca253c30b1a2079df2fd8ac8043d03
Johan Martinsson <martinsson.johan@gmail.com>
2015-04-07 00:11:36 +0200

    created subclass

---
+++ java/src/test/java/tennis/TieBreak.java
@@ -0,0 +1,10 @@
+package tennis;
+
+public class TieBreak extends Tennis {
+
+    public TieBreak(Tennis tennis) {
+	this.playerABalls = tennis.playerABalls;
+	this.playerBBalls = tennis.playerBBalls;
+    }
+
+}
