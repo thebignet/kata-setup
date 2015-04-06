6af88a09d9e7c3011db8622d64ca32123e021da0
Johan Martinsson <martinsson.johan@gmail.com>
2015-04-06 23:54:14 +0200

    experimenting with conditionals

--- java/src/test/java/tennis/Tennis.java
+++ java/src/test/java/tennis/Tennis.java
@@ -8,11 +8,11 @@ public class Tennis {
     }
 
     void playerBScores() {
-        playerBBalls++;
+	playerBBalls++;
     }
 
     void playerAScores() {
-        playerABalls++;
+	playerABalls++;
     }
 
     String score() {
@@ -20,27 +20,35 @@ public class Tennis {
     }
 
     private String format(int aScore, int bScore) {
-	if (aScore >= 3) {
-	    if (aScore-bScore == -2)
+	if (inBeginningOfGame(aScore, bScore)) {
+	    if (aScore == 4)
+		return "game-A";
+	    if (bScore == 4)
 		return "game-B";
-	    if (aScore-bScore == -1)
+	    return format(aScore) + "-" + format(bScore);
+	} else {
+	    if (aScore - bScore == -2)
+		return "game-B";
+	    if (aScore - bScore == -1)
 		return "advantage-B";
-	    if (aScore-bScore == 1)
+	    if (aScore - bScore == 1)
 		return "advantage-A";
 	    if (aScore - bScore == 0)
 		return "deuce";
-	    if (aScore-bScore == 2)
+	    if (aScore - bScore == 2)
 		return "game-A";
+	    else
+		throw new RuntimeException("cant happen");
 	}
-	
-	if (aScore == 4) return "game-A";
-	if (bScore == 4) return "game-B";
-	if (bScore == 3 && aScore == 3) return "deuce";
-	return format(aScore) + "-" + format(bScore);
+
+    }
+
+    private boolean inBeginningOfGame(int aScore, int bScore) {
+	return aScore + bScore < 6;
     }
 
     private String format(int score) {
-	String[] scores = {"love", "fifteen", "thirty", "forty"};
+	String[] scores = { "love", "fifteen", "thirty", "forty" };
 	return scores[score];
     }
 }
