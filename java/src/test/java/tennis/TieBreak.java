2bc448c8b561c4804880a516132b9edc29f162fe
Johan Martinsson <martinsson.johan@gmail.com>
2015-04-07 00:30:38 +0200

    using map in TieBreak

--- java/src/test/java/tennis/TieBreak.java
+++ java/src/test/java/tennis/TieBreak.java
@@ -1,5 +1,9 @@
 package tennis;
 
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+
 public class TieBreak extends Tennis {
 
     public TieBreak(Tennis tennis) {
@@ -9,18 +13,15 @@ public class TieBreak extends Tennis {
 
     @Override
     public String score() {
-	if (playerABalls - playerBBalls == -2)
-	    return "game-B";
-	if (playerABalls - playerBBalls == -1)
-	    return "advantage-B";
-	if (playerABalls - playerBBalls == 1)
-	    return "advantage-A";
-	if (playerABalls - playerBBalls == 0)
-	    return "deuce";
-	if (playerABalls - playerBBalls == 2)
-	    return "game-A";
-	else
-	    throw new RuntimeException("cant happen");
+	Map<Integer, String> relativeScore = ImmutableMap.of(
+		-2, "game-B",
+		-1, "advantage-B",
+		 0, "deuce",
+		 1, "advantage-A",
+		 2, "game-A"
+		);
+	    int scoreDifference = playerABalls - playerBBalls;
+	    return relativeScore.get(scoreDifference);
     }
 
 }
