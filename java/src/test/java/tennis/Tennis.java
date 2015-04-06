f71cef5595d28f550f4eefd18c05cd9cb251a8b7
Johan Martinsson <martinsson.johan@gmail.com>
2015-04-06 22:56:52 +0200

    introducing formatter

--- java/src/test/java/tennis/Tennis.java
+++ java/src/test/java/tennis/Tennis.java
@@ -18,9 +18,15 @@ public class Tennis {
     String score() {
 	if (playerBBalls == 2) return "love-thirty";
         if (playerBBalls == 1) return "love-fifteen";
+        
         if (playerABalls == 2) return "thirty-love";
         if (playerABalls == 1) return "fifteen-love";
-        if (playerABalls == 0) return "love-love";
+        if (playerABalls == 0) return format(0, 0);
+        
         else throw new RuntimeException("not implemented");
+    }
+
+    private String format(int aScore, int bScore) {
+	return "love-love";
     }
 }
