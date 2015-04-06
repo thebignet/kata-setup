10cbec7077cfc4ebb66fdda86757fa49df0192a9
Johan Martinsson <martinsson.johan@gmail.com>
2015-04-07 00:15:01 +0200

    implemented subclass and simplified

--- java/src/test/java/tennis/Tennis.java
+++ java/src/test/java/tennis/Tennis.java
@@ -24,26 +24,11 @@ public class Tennis {
     }
 
     protected String format() {
-	if (inBeginningOfGame()) {
-	    if (playerABalls == 4)
-		return "game-A";
-	    if (playerBBalls == 4)
-		return "game-B";
-	    return format(playerABalls) + "-" + format(playerBBalls);
-	} else {
-	    if (playerABalls - playerBBalls == -2)
-		return "game-B";
-	    if (playerABalls - playerBBalls == -1)
-		return "advantage-B";
-	    if (playerABalls - playerBBalls == 1)
-		return "advantage-A";
-	    if (playerABalls - playerBBalls == 0)
-		return "deuce";
-	    if (playerABalls - playerBBalls == 2)
-		return "game-A";
-	    else
-		throw new RuntimeException("cant happen");
-	}
+	if (playerABalls == 4)
+	    return "game-A";
+	if (playerBBalls == 4)
+	    return "game-B";
+	return format(playerABalls) + "-" + format(playerBBalls);
 
     }
 
@@ -51,7 +36,7 @@ public class Tennis {
 	return playerABalls + playerBBalls < 6;
     }
 
-    private String format(int score) {
+    protected String format(int score) {
 	String[] scores = { "love", "fifteen", "thirty", "forty" };
 	return scores[score];
     }
--- java/src/test/java/tennis/TieBreak.java
+++ java/src/test/java/tennis/TieBreak.java
@@ -6,7 +6,21 @@ public class TieBreak extends Tennis {
 	this.playerABalls = tennis.playerABalls;
 	this.playerBBalls = tennis.playerBBalls;
     }
-    
-    
+
+    @Override
+    protected String format() {
+	if (playerABalls - playerBBalls == -2)
+	    return "game-B";
+	if (playerABalls - playerBBalls == -1)
+	    return "advantage-B";
+	if (playerABalls - playerBBalls == 1)
+	    return "advantage-A";
+	if (playerABalls - playerBBalls == 0)
+	    return "deuce";
+	if (playerABalls - playerBBalls == 2)
+	    return "game-A";
+	else
+	    throw new RuntimeException("cant happen");
+    }
 
 }
