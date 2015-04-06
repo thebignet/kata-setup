fa477996a08e1dab87a8a6708d902fc6c5791bf8
Johan Martinsson <martinsson.johan@gmail.com>
2015-04-06 23:01:39 +0200

    formatters 30

--- java/src/test/java/tennis/Tennis.java
+++ java/src/test/java/tennis/Tennis.java
@@ -16,10 +16,10 @@ public class Tennis {
     }
 
     String score() {
-	if (playerBBalls == 2) return "love-thirty";
-        if (playerBBalls == 1) return "love-fifteen";
+	if (playerBBalls == 2) return format(0, 2);
+        if (playerBBalls == 1) return format(0, 1);
         
-        if (playerABalls == 2) return "thirty-love";
+        if (playerABalls == 2) return format(2, 0);
         if (playerABalls == 1) return format(1, 0);
         if (playerABalls == 0) return format(0, 0);
         
@@ -31,6 +31,7 @@ public class Tennis {
     }
 
     private String format(int score) {
+	if (score == 2) return "thirty" ;
 	if (score == 1) return "fifteen" ;
 	return "love";
     }
