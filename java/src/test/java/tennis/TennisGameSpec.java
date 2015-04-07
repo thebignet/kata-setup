2b70159e6fa3c894dd4282c78bd413a31f9ab035
Johan Martinsson <martinsson.johan@gmail.com>
2015-04-07 20:13:25 +0200

    array of scores

--- java/src/test/java/tennis/TennisGameSpec.java
+++ java/src/test/java/tennis/TennisGameSpec.java
@@ -49,10 +49,8 @@ public class TennisGameSpec {
     }
 
     private String format(int score) {
-	if (score == 3) return "forty";
-	if (score == 2) return "thirty";
-	if (score == 1) return "fifteen";
-	return "love";
+	String[] scores = {"love", "fifteen", "thirty", "forty"};
+	return scores[score];
     }
 
 }
