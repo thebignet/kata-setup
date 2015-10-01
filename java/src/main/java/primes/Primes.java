204b824aa41f20010d52dc155b6699dbc90d47e1
Johan Martinsson <martinsson.johan@gmail.com>
2015-10-01 14:42:13 +0200

    finished implementation

--- java/src/main/java/primes/Primes.java
+++ java/src/main/java/primes/Primes.java
@@ -1,12 +1,29 @@
 package primes;
 
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Primes {
 
   public List<Integer> factorsOf(int number) {
-    return Arrays.asList(number);
+    ArrayList<Integer> factors = new ArrayList<>();
+    
+    
+    int potentialPrime = 2;
+    
+    for (int i = 0; i < number; i++) {
+      while(number % potentialPrime == 0) {
+        factors.add(potentialPrime);
+        number = number / potentialPrime;
+      }
+      potentialPrime++;
+      
+    }
+    
+    return factors;
   }
 
 }
--- java/src/test/java/primes/PrimesTest.java
+++ java/src/test/java/primes/PrimesTest.java
@@ -1,6 +1,7 @@
 package primes;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
@@ -12,16 +13,31 @@ public class PrimesTest {
   private Primes primes = new Primes();
 
   @Test
-  public void factorsOfPrimesIsJustThePrime() {
+  public void factors_Of_Primes_Is_Just_The_Prime() {
     assertThat(factorsOf(2)).containsExactly(2);
     assertThat(factorsOf(3)).containsExactly(3);
   }
   
-  @Ignore @Test public void 
-  acceptanceTest() throws Exception {
-    assertThat(factorsOf(2 * 3 * 3 * 29 * 7)).containsExactly(2, 3, 3, 29, 7);
-     
+  @Test public void 
+  /**
+   * numbers Containing Twice One Prime Decompose To Twice The Prime
+   * @throws Exception
+   */
+  numbers_Containing_XTimes_One_Prime_Decompose_To_XTimes_The_Prime() throws Exception {
+    assertThat(factorsOf(4)).containsExactly(2, 2);
+    assertThat(factorsOf(8)).containsExactly(2, 2, 2);
+    assertThat(factorsOf(9)).containsExactly(3, 3);
+    assertThat(factorsOf(27)).containsExactly(3, 3, 3);
+    assertThat(factorsOf(25)).containsExactly(5, 5);
   }
+  
+  
+  @Test public void 
+  acceptanceTest() throws Exception {
+    assertThat(factorsOf(2 * 3 * 3 * 29 * 7)).contains(2, 3, 3, 29, 7);
+    assertTrue("frreddd".matches("f*r*e*d*"));
+  }
+  
 
   private List<Integer> factorsOf(int number) {
     return primes.factorsOf(number);
