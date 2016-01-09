61ec40ad01ce4696ab1f67212c89abce20c15f3d
Johan Martinsson <martinsson.johan@gmail.com>
2016-01-09 01:19:04 +0100

    attempted ordinary functional validators

--- java/src/test/java/kata/PasswordChecker.java
+++ java/src/test/java/kata/PasswordChecker.java
@@ -4,6 +4,9 @@ import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 import kata.Validator.DigitValidator;
 import kata.Validator.LengthValidator;
@@ -12,36 +15,51 @@ import kata.Validator.SpecialCharValidator;
 
 public class PasswordChecker {
 
-  public List<String> check(String password) {
-    List<Validator> validators = asList(
-        new LengthValidator(7), 
-        new DigitValidator(), 
-        new LetterValidator());
+    public List<String> check(String password) {
+        List<Validator> validators = asList(
+                new LengthValidator(7),
+                new DigitValidator(),
+                new LetterValidator());
 
-    return validate(password, validators);
-  }
-  
-  public List<String> checkAdmin(String password) {
-    
-    return validate(password, adminValidators);
-  }
-
-  private List<Validator> adminValidators = asList(
-      new LengthValidator(10), 
-      new DigitValidator(), 
-      new LetterValidator(),
-      new SpecialCharValidator());
-
-  protected List<String> validate(String password, List<Validator> validators) {
-    List<String> errors = emptyList();
-    for (Validator v : validators ) {
-      v.validate(password, errors);
+        return validate(password, validators);
     }
-    return errors;
-  }
 
-  private ArrayList<String> emptyList() {
-    return new ArrayList<String>();
-  }
+    public List<String> checkAdmin(String password) {
+
+        return validate(password, adminValidators);
+    }
+
+    private List<Validator> adminValidators = asList(
+            new LengthValidator(10),
+            new DigitValidator(),
+            new LetterValidator(),
+            new SpecialCharValidator());
+
+//    protected List<String> validateF(String password, Stream<Function<String, Optional<String>>> validators) {
+//        return validators.map((Function<String, Optional<String>> v) -> v.apply(password));
+//
+//    }
+
+
+    protected List<String> validate(String password, List<Validator> validators) {
+
+
+        List<String> errors = emptyList();
+        for (Validator v : validators) {
+            errors.add(v.validate(password));
+        }
+        return errors;
+    }
+
+    private ArrayList<String> emptyList() {
+        return new ArrayList<String>() {
+            @Override
+            public boolean add(String s) {
+                if (s.isEmpty())
+                    return false;
+                else return super.add(s);
+            }
+        };
+    }
 
 }
--- java/src/test/java/kata/Validator.java
+++ java/src/test/java/kata/Validator.java
@@ -3,58 +3,81 @@ package kata;
 import java.util.List;
 
 interface Validator {
-  void validate(String password, List<String> errors);
+    void validate(String password, List<String> errors);
 
-  public final class LetterValidator implements Validator {
-    @Override
-    public void validate(String password, List<String> errors) {
-      if (!containsLetter(password))
-        errors.add("Password must contain a letter");
+    String validate(String password);
+
+    public final class LetterValidator implements Validator {
+        @Override
+        public void validate(String password, List<String> errors) {
+            if (!containsLetter(password))
+                errors.add("Password must contain a letter");
+        }
+
+        @Override
+        public String validate(String password) {
+            return containsLetter(password) ? "" : "Password must contain a letter";
+        }
+
+
+        private boolean containsLetter(String password) {
+            return password.matches(".*[a-zA-Z].*");
+        }
+
+
     }
 
-    private boolean containsLetter(String password) {
-      return password.matches(".*[a-zA-Z].*");
-    }
+    public class DigitValidator implements Validator {
 
+        @Override
+        public void validate(String password, List<String> errors) {
+            if (!password.matches(".*\\d.*"))
+                errors.add("Password needs to contain atleast one digit");
+        }
 
-  }
-
-  public class DigitValidator implements Validator {
-
-    @Override
-    public void validate(String password, List<String> errors) {
-      if (! password.matches(".*\\d.*"))
-        errors.add("Password needs to contain atleast one digit");
-    }
-
-  }
-
-  public class SpecialCharValidator implements Validator {
-
-    @Override
-    public void validate(String password, List<String> errors) {
-      if (!containsSpecialCharacter(password))
-        errors.add("Password needs to contain atleast one special character");
-    }
-
-    private boolean containsSpecialCharacter(String password) {
-      return password.matches(".*[\\.!?].*");
-    }
-
-
-  }
-
-  public final class LengthValidator implements Validator {
-    private int length;
-
-    public LengthValidator(int length) {
-      this.length = length;
-    }
-
-    public void validate(String password, List<String> errors) {
-      if (password.length() < length)
-        errors.add("Password needs to be " + length + " chars long");
+        @Override
+        public String validate(String password) {
+            return password.matches(".*\\d.*") ? "" : "Password needs to contain atleast one digit";
+        }
 
     }
-  }
+
+    public class SpecialCharValidator implements Validator {
+
+        @Override
+        public void validate(String password, List<String> errors) {
+            if (!containsSpecialCharacter(password))
+                errors.add("Password needs to contain atleast one special character");
+        }
+
+        @Override
+        public String validate(String password) {
+            return containsSpecialCharacter(password) ? "" : "Password needs to contain atleast one special character";
+        }
+
+        private boolean containsSpecialCharacter(String password) {
+            return password.matches(".*[\\.!?].*");
+        }
+
+
+    }
+
+    public final class LengthValidator implements Validator {
+        private int length;
+
+        public LengthValidator(int length) {
+            this.length = length;
+        }
+
+        public void validate(String password, List<String> errors) {
+            if (password.length() < length)
+                errors.add("Password needs to be " + length + " chars long");
+
+        }
+
+        @Override
+        public String validate(String password) {
+            return password.length() < length ? "Password needs to be " + length + " chars long" : "";
+        }
+    }
 }
