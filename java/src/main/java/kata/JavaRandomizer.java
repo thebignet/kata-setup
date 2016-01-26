932759636c4342d06462bafca0d2db0743d62210
Johan Martinsson <martinsson.johan@gmail.com>
2016-01-27 00:32:42 +0100

    extracted implementation of Ranomizer

---
+++ java/src/main/java/kata/JavaRandomizer.java
@@ -0,0 +1,13 @@
+package kata;
+
+import java.util.Random;
+
+public final class JavaRandomizer implements Randomizer {
+	public static final int UPPERLIMIT_FOR_RANDOM_36 = 38;
+	private Random random = new Random();
+	
+	@Override
+	public int getRouletteResult() {
+		return random.nextInt(UPPERLIMIT_FOR_RANDOM_36);
+	}
+}
