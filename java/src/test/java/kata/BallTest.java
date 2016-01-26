f0d9de5c1936eabaa93d0d7d894dced37ae740bb
Johan Martinsson <martinsson.johan@gmail.com>
2016-01-27 00:17:15 +0100

    fix timing that was a bit short

--- java/src/test/java/kata/BallTest.java
+++ java/src/test/java/kata/BallTest.java
@@ -9,14 +9,14 @@ public class BallTest {
 
   @Test
   public void itRollsForAGivenAmountOfTime() throws Exception {
-    int timeInMilliseconds = 20;
+    int timeInMilliseconds = 70;
     Ball ball = new Ball(timeInMilliseconds);
 
     double before = System.currentTimeMillis();
     ball.roll();
     double after = System.currentTimeMillis();
 
-    assertThat(after - before).isCloseTo(20, within(3d));
+    assertThat(after - before).isCloseTo(70, within(30d));
 
   }
 }
