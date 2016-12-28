bd5a6183f190544a513fb420b267fc5d9b4fe514
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-12-28 23:28:11 +0100

    create a main method, and validation

--- java/src/main/java/kata/Main.java
+++ java/src/main/java/kata/Main.java
@@ -1,7 +1,20 @@
 package kata;
 
+import java.util.Scanner;
+
 /**
  * Created by johan on 28/12/16.
  */
 public class Main {
+
+
+    public static void main(String[] args) {
+        String input = new Scanner(System.in).nextLine();
+        int number = validateInputIsNumber(input);
+        new Lcd().display(number);
+    }
+
+    private static Integer validateInputIsNumber(String input) {
+        return Integer.valueOf(input);
+    }
 }
