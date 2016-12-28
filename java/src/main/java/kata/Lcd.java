c7266050f678b36a128dee2de3199fee851440b6
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-12-28 22:15:49 +0100

    fix indentation

--- java/src/main/java/kata/Lcd.java
+++ java/src/main/java/kata/Lcd.java
@@ -6,36 +6,36 @@ import java.util.Map;
 public class Lcd {
 
     @SuppressWarnings("serial")
-    private Map<Character, String[]> cases = new HashMap<Character,String[]>() {{
-	put('1', new String[] { "   ", "  |", "  |" });
-	put('2', new String[] { " _ ", " _|", "|_ " });
-	put('7', new String[] { " _ ", "  |", "  |" });
+    private Map<Character, String[]> cases = new HashMap<Character, String[]>() {{
+        put('1', new String[]{"   ", "  |", "  |"});
+        put('2', new String[]{" _ ", " _|", "|_ "});
+        put('7', new String[]{" _ ", "  |", "  |"});
     }};
-    
-    private static final Integer[] lines = {0, 1, 2};		
+
+    private static final Integer[] lines = {0, 1, 2};
     private static final String NL = "\n";
 
     public String display(int i) {
-	return display(String.valueOf(i));
+        return display(String.valueOf(i));
     }
 
     protected String display(String number) {
-	return display(number.toCharArray());
+        return display(number.toCharArray());
     }
 
     protected String display(char[] digits) {
-	StringBuilder s = new StringBuilder();
-	for (int line : lines) {
-	    for (char digit : digits) {
-		s.append(lcdDigit(digit, line));
-	    }
-	    s.append(NL);
-	}
-	return s.toString();
+        StringBuilder s = new StringBuilder();
+        for (int line : lines) {
+            for (char digit : digits) {
+                s.append(lcdDigit(digit, line));
+            }
+            s.append(NL);
+        }
+        return s.toString();
     }
 
     protected String lcdDigit(char digit, int line) {
-	return cases.get(digit)[line];
+        return cases.get(digit)[line];
     }
 
 }
--- java/src/test/java/kata/LcdTest.java
+++ java/src/test/java/kata/LcdTest.java
@@ -14,62 +14,63 @@ public class LcdTest {
         Lcd lcd = new Lcd();
         String result = lcd.display(1);
         Assertions.assertThat(result).isEqualTo(
-        		"   \n" + 
-        		"  |\n" + 
+        		"   \n" +
+        		"  |\n" +
         		"  |\n" );
-        	
+
     }
     @Test
     public void for_7() {
-	Lcd lcd = new Lcd();
-	String result = lcd.display(7);
-	Assertions.assertThat(result).isEqualTo(
-		" _ " + NL + 
-		"  |" + NL + 
-		"  |" + NL );
+        Lcd lcd = new Lcd();
+        String result = lcd.display(7);
+        Assertions.assertThat(result).isEqualTo(
+            " _ " + NL +
+            "  |" + NL +
+            "  |" + NL );
     }
-    
+
     @Test
     public void for_2() {
-	Lcd lcd = new Lcd();
-	String result = lcd.display(22);
-	Assertions.assertThat(result).isEqualTo(
-		" _ " + " _ " + NL + 
-		" _|" + " _|" + NL + 
-		"|_ " + "|_ " + NL );
+        Lcd lcd = new Lcd();
+        String result = lcd.display(22);
+        Assertions.assertThat(result).isEqualTo(
+            " _ " + " _ " + NL +
+            " _|" + " _|" + NL +
+            "|_ " + "|_ " + NL );
     }
+
     @Test
     public void for_77() {
-	Lcd lcd = new Lcd();
-	String result = lcd.display(77);
-	Assertions.assertThat(result).isEqualTo(
-		" _ " + " _ " + NL + 
-		"  |" + "  |" + NL + 
-		"  |" + "  |" + NL );
+        Lcd lcd = new Lcd();
+        String result = lcd.display(77);
+        Assertions.assertThat(result).isEqualTo(
+            " _ " + " _ " + NL +
+            "  |" + "  |" + NL +
+            "  |" + "  |" + NL );
     }
-    
+
     @Test
     public void for_172() {
-	Lcd lcd = new Lcd();
-	String result = lcd.display(172);
-	Assertions.assertThat(result).isEqualTo(
-		"   " + " _ " + " _ " + NL + 
-		"  |" + "  |" + " _|" + NL + 
-		"  |" + "  |" + "|_ " + NL );
-	System.out.println(result);
+        Lcd lcd = new Lcd();
+        String result = lcd.display(172);
+        Assertions.assertThat(result).isEqualTo(
+            "   " + " _ " + " _ " + NL +
+            "  |" + "  |" + " _|" + NL +
+            "  |" + "  |" + "|_ " + NL );
+        System.out.println(result);
     }
-    
-    @Test public void 
-    div() throws Exception {
-	assertThat(21 / 2).isEqualTo(10); 
-	assertThat(21 % 10).isEqualTo(1); 
-	assertThat(572 % 10).isEqualTo(2); 
-	assertThat(572 / 10).isEqualTo(57); 
-	assertThat(572 / 100).isEqualTo(5); 
-	assertThat(572 % 100).isEqualTo(72); 
-	assertThat(Math.floor(Math.log10(12345))).isEqualTo(4);
-	assertThat(String.valueOf(12345).length()).isEqualTo(5);
-	assertThat(Math.pow(10,3)).isEqualTo(1000);
+
+    @Test public void
+        div() throws Exception {
+        assertThat(21 / 2).isEqualTo(10);
+        assertThat(21 % 10).isEqualTo(1);
+        assertThat(572 % 10).isEqualTo(2);
+        assertThat(572 / 10).isEqualTo(57);
+        assertThat(572 / 100).isEqualTo(5);
+        assertThat(572 % 100).isEqualTo(72);
+        assertThat(Math.floor(Math.log10(12345))).isEqualTo(4);
+        assertThat(String.valueOf(12345).length()).isEqualTo(5);
+        assertThat(Math.pow(10,3)).isEqualTo(1000);
     }
-    
+
 }
