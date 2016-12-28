28bea875cf9d673b486b90d8e62effafb0a0dc66
Johan MARTINSSON <martinsson.johan@gmail.com>
2016-12-28 22:28:55 +0100

    rename lines to lineNumber introduce Digit class

--- java/src/main/java/kata/Lcd.java
+++ java/src/main/java/kata/Lcd.java
@@ -1,18 +1,31 @@
 package kata;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Lcd {
 
+    private class Digit {
+        private List<String> lines;
+
+        public Digit(String...lines) {
+            this.lines = Arrays.asList(lines);
+        }
+
+        public String forLine(int lineNumber) {
+            return lines.get(lineNumber);
+        }
+    }
     @SuppressWarnings("serial")
-    private Map<Character, String[]> cases = new HashMap<Character, String[]>() {{
-        put('1', new String[]{"   ", "  |", "  |"});
-        put('2', new String[]{" _ ", " _|", "|_ "});
-        put('7', new String[]{" _ ", "  |", "  |"});
+    private Map<Character, Digit> cases = new HashMap<Character, Digit>() {{
+        put('1', new Digit("   ", "  |", "  |"));
+        put('2', new Digit(" _ ", " _|", "|_ "));
+        put('7', new Digit(" _ ", "  |", "  |"));
     }};
 
-    private static final Integer[] lines = {0, 1, 2};
+    private static final Integer[] lineNumbers = {0, 1, 2};
     private static final String NL = "\n";
 
     public String display(int i) {
@@ -25,9 +38,9 @@ public class Lcd {
 
     protected String display(char[] digits) {
         StringBuilder s = new StringBuilder();
-        for (int line : lines) {
+        for (int lineNumber : lineNumbers) {
             for (char digit : digits) {
-                s.append(lcdDigit(digit, line));
+                s.append(lcdDigit(digit, lineNumber));
             }
             s.append(NL);
         }
@@ -35,7 +48,7 @@ public class Lcd {
     }
 
     protected String lcdDigit(char digit, int line) {
-        return cases.get(digit)[line];
+        return cases.get(digit).forLine(line);
     }
 
 }
