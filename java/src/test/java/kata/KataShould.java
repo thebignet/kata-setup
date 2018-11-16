8225b73baef96860ebcbd51d59f8942dbd6062a7
Jean Detoeuf <jean@upcoding.fr>
2018-11-16 21:50:19 +0100

    added parametrized java test

--- java/src/test/java/kata/KataShould.java
+++ java/src/test/java/kata/KataShould.java
@@ -2,13 +2,21 @@ package kata;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class KataShould {
 
     @Test
     public void startWithAFailingTest() {
         Assertions.fail("start by implementing a test");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "Hello", "JUnit" })
+    void withValueSource(String word) {
+        assertNotNull(word);
     }
 }
