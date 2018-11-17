77a49714f89e7dadba769ab58c6af19bf5f5c2a5
Jean Detoeuf <jean@upcoding.fr>
2018-11-17 13:43:44 +0100

    avancement

--- scala/src/test/scala/KataSpec.scala
+++ scala/src/test/scala/KataSpec.scala
@@ -2,10 +2,10 @@ import org.scalatest._
 
 class ExampleSpec extends FlatSpec with Matchers {
 
-  def isPair = true
+  def isPair(card1:String, card2: String) = true
 
   it should "be a pair when cards have same value" in {
-    isPair should be(true)
+    isPair("1", "2") should be(true)
   }
 
 }
