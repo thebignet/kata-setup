7cc8dcb88b8f152d333b911293c937128afe50c7
Jean Detoeuf <jean@upcoding.fr>
2018-11-17 11:07:22 +0100

    avec remi au globalday

--- scala/src/main/scala/Kata.scala
+++ scala/src/main/scala/Kata.scala
@@ -1,3 +1,28 @@
-class Kata {
-  def method(input: String) = Integer.valueOf(input)
+
+
+sealed abstract class Card(val value: Int)
+case object Ace extends Card(100)
+case object Two extends Card(2)
+case object Three extends Card(3)
+case class Hand(card1: Card, card2: Card) {
+  def bestCombination(): Combination = if (card1 == card2) PokerPair(card1)
+  else {
+    if (card1.value > card2.value) HighCard(card1) else HighCard(card2)
+  }
+}
+
+sealed trait Combination
+case class PokerPair(card: Card) extends Combination
+case class HighCard(card: Card) extends Combination
+
+case class Round(hand1: Hand, hand2: Hand) {
+  def winner(): Hand = (hand1.bestCombination(), hand2.bestCombination()) match {
+    case (PokerPair(card1), PokerPair(card2)) if card1.value > card2.value => hand1
+    case (PokerPair(card1), PokerPair(card2)) if card1.value <= card2.value => hand2
+    case (PokerPair(_), _) => hand1
+    case (_, PokerPair(_)) => hand2
+    case (HighCard(card1), HighCard(card2)) if card1.value > card2.value => hand1
+    case (HighCard(card1), HighCard(card2)) if card1.value > card2.value => hand2
+  }
+
 }
--- scala/src/test/scala/KataSpec.scala
+++ scala/src/test/scala/KataSpec.scala
@@ -2,7 +2,7 @@ import org.scalatest._
 
 class ExampleSpec extends FlatSpec with Matchers {
 
-  "Kata" should "convert string of 1 to number 1" in {
+  /*"Kata" should "convert string of 1 to number 1" in {
     val kata = new Kata
     val result = kata.method("1")
     result should be(1)
@@ -21,6 +21,48 @@ class ExampleSpec extends FlatSpec with Matchers {
 
   ignore should "show how to ignore a test" in {
     succeed
+  }*/
+
+  it should "be a pair when two card with same value" in {
+    val hand = Hand(Ace, Ace)
+    hand.bestCombination() match {
+      case PokerPair(Ace) => succeed
+      case _ => fail
+    }
   }
 
+  it should "be a high card when two card have different values" in {
+    val hand = Hand(Ace, Two)
+    hand.bestCombination() match {
+      case HighCard(Ace) => succeed
+      case _ => fail
+    }
+  }
+
+  "the winner hand between a pair and a high card" should "be a pair" in {
+    val handWithPair = Hand(Ace, Ace)
+    val handWithHighCard = Hand(Ace, Two)
+
+    val winner = Round(handWithHighCard, handWithPair).winner()
+
+    winner should be(handWithPair)
+  }
+
+  "the winner between two high cards" should "be the one with the highest" in {
+    val handWithHighest = Hand(Ace, Two)
+    val handWithLowest = Hand(Two, Three)
+
+    val winner = Round(handWithHighest, handWithLowest).winner()
+
+    winner should be(handWithHighest)
+  }
+
+  "the winner between two pairs" should "be the one with the highest" in {
+    val handWithHighest = Hand(Ace, Ace)
+    val handWithLowest = Hand(Two, Two)
+
+    val winner = Round(handWithLowest, handWithHighest).winner()
+
+    winner should be(handWithHighest)
+  }
 }
