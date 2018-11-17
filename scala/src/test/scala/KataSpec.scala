79893704dd0078482846f5effe896f838d23f5e3
Jean Detoeuf <jean@upcoding.fr>
2018-11-17 13:53:37 +0100

    avancement

--- scala/src/test/scala/KataSpec.scala
+++ scala/src/test/scala/KataSpec.scala
@@ -2,9 +2,9 @@ import org.scalatest._
 
 class ExampleSpec extends FlatSpec with Matchers {
 
-  sealed trait Card
-  case class Ace() extends Card
-  case class King() extends Card
+  abstract class Card(val value: Int)
+  case class Ace() extends Card(2)
+  case class King() extends Card(1)
   def isPair(card1:Card, card2: Card) = card1 == card2
 
   it should "be a pair when cards have same value" in {
@@ -15,10 +15,11 @@ class ExampleSpec extends FlatSpec with Matchers {
     isPair(Ace(), King()) should be(false)
   }
 
-  def getHighcard(cards: List[Card]) = cards.tail.head
+  def getHighcard(cards: List[Card]) = cards.maxBy(_.value)
 
   it should "get high card" in {
     getHighcard(List(King(), Ace())) should be(Ace())
+    getHighcard(List(Ace(), King())) should be(Ace())
   }
 
 }
