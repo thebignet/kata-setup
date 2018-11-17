14285452bc13c1dbaa46e43822ce835f895d92b4
Jean Detoeuf <jean@upcoding.fr>
2018-11-17 13:40:55 +0100

    avancement

--- scala/src/test/scala/KataSpec.scala
+++ scala/src/test/scala/KataSpec.scala
@@ -2,7 +2,7 @@ import org.scalatest._
 
 class ExampleSpec extends FlatSpec with Matchers {
 
-  it should "start with a failing test" in {
+  it should "be a pair when cards have same value" in {
     succeed
   }
 
