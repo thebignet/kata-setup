3fa117cfffc6d9d9b4dd8cb938d736ac8f78a3a2
Jean Detoeuf <jean@upcoding.fr>
2018-11-17 14:14:07 +0100

    avancement

--- scala/src/test/scala/KataSpec.scala
+++ scala/src/test/scala/KataSpec.scala
@@ -16,17 +16,17 @@ class ExampleSpec extends FlatSpec with Matchers {
     isPair(Ace(), King()) should be(false)
   }
 
-  def getHighcard(cards: List[Card]) = cards.maxBy(_.value)
+  def getHighcard(cards: List[Card]) = cards.maxBy(_.value).value
 
   it should "get high card" in {
-    getHighcard(List(King(), Ace())) should be(Ace())
-    getHighcard(List(Ace(), King())) should be(Ace())
+    getHighcard(List(King(), Ace())) should be(Ace().value)
+    getHighcard(List(Ace(), King())) should be(Ace().value)
   }
 
   def getWinner(card1Player1: Card, card2Player1: Card, card1Player2: Card, card2Player2: Card) =
     if(isPair(card1Player1,card2Player1)) Player1
     else if (isPair(card1Player2, card2Player2)) Player2
-    else if(getHighcard(List(card1Player1, card2Player1)).value > getHighcard(List(card1Player2, card2Player2)).value) Player1
+    else if(getHighcard(List(card1Player1, card2Player1)) > getHighcard(List(card1Player2, card2Player2))) Player1
     else Player2
 
   it should "get player when player has pair" in {
