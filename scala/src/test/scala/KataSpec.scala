b69a3093221d3dd0a4a79051b0abba63885385bd
Jean Detoeuf <jean@upcoding.fr>
2018-11-17 13:37:11 +0100

    avancement

--- scala/src/test/scala/KataSpec.scala
+++ scala/src/test/scala/KataSpec.scala
@@ -2,19 +2,6 @@ import org.scalatest._
 
 class ExampleSpec extends FlatSpec with Matchers {
 
-  /*"Kata" should "convert string of 1 to number 1" in {
-    val kata = new Kata
-    val result = kata.method("1")
-    result should be(1)
-  }
-
-  it should "throw NumberFormatException if input is not a number" in {
-    val kata = new Kata
-    a[NumberFormatException] should be thrownBy {
-      kata.method("a")
-    }
-  }*/
-
   it should "start with a failing test" in {
     succeed
   }
