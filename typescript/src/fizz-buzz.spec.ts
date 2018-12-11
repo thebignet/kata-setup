1e3a55c15a026bebf6094b776e92ae1d46071783
Jean Detoeuf <jdetoeuf.ext@orange.com>
2018-12-11 09:02:15 +0100

    ✨ solution fizz-buzz

--- typescript/src/fizz-buzz.spec.ts
+++ typescript/src/fizz-buzz.spec.ts
@@ -1,4 +1,4 @@
-import { of } from './fizz-buzz';
+import { of, until } from './fizz-buzz';
 import { expect } from 'chai';
 import 'mocha';
 
@@ -7,6 +7,46 @@ describe('FizzBuzz', () => {
   it('1 should return 1', () => {
     const result = of(1);
     expect(result).to.equal('1');
+  });
+
+  it('2 should return 2', () => {
+    const result = of(2);
+    expect(result).to.equal('2');
+  });
+
+  it('3 should return Fizz', () => {
+    const result = of(3);
+    expect(result).to.equal('Fizz');
+  });
+
+  it('5 should return Buzz', () => {
+    const result = of(5);
+    expect(result).to.equal('Buzz');
+  });
+
+  it('6 should return Fizz', () => {
+    const result = of(6);
+    expect(result).to.equal('Fizz');
+  });
+
+  it('10 should return Buzz', () => {
+    const result = of(10);
+    expect(result).to.equal('Buzz');
+  });
+
+  it('15 should return FizzBuzz', () => {
+    const result = of(15);
+    expect(result).to.equal('FizzBuzz');
+  });
+
+  it('30 should return FizzBuzz', () => {
+    const result = of(30);
+    expect(result).to.equal('FizzBuzz');
+  });
+
+  it('FizzBuzz from 0 to 15', () => {
+    const result = until(15);
+    expect(result).to.equal('1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz');
   });
 
 });
--- typescript/src/fizz-buzz.ts
+++ typescript/src/fizz-buzz.ts
@@ -1,3 +1,14 @@
-export function of(num){
-  return "1";
+export function of(num): String{
+  var r = '';
+  if(num % 3 == 0) {
+    r += "Fizz";
+  }
+  if(num % 5 == 0) {
+    r += "Buzz";
+  }
+  return r !== '' ? r : String(num);
+}
+
+export function until(num): String {
+  return [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15].map(n => of(n)).join(' ')
 }
