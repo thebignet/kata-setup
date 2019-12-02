d4537edd61d24c9562b9c5eac3400d2c5df9fbfc
Jean Detoeuf <jean@upcoding.fr>
2019-12-02 19:43:11 +0100

    letters B and C

--- scala/src/main/scala/Diamond.scala
+++ scala/src/main/scala/Diamond.scala
@@ -1,7 +1,21 @@
 
 
 class Diamond {
-	def apply(letter: Char): Seq[Seq[Char]] = Seq(Seq('A'))
+	def apply(letter: Char): Seq[Seq[Char]] = letter match {
+		case 'A' => Seq(Seq('A'))
+		case 'B' => Seq(
+			Seq(' ', 'A', ' '),
+			Seq('B', ' ', 'B'),
+			Seq(' ', 'A', ' ')
+		)
+		case 'C' => Seq(
+			Seq(' ', ' ', 'A', ' ', ' '),
+			Seq(' ', 'B', ' ', 'B', ' '),
+			Seq('C', ' ', ' ', ' ', 'C'),
+			Seq(' ', 'B', ' ', 'B', ' '),
+			Seq(' ', ' ', 'A', ' ', ' '),
+		)
+	}
 
 	def method(input: String) = Integer.valueOf(input)
 }
--- scala/src/test/scala/KataSpec.scala
+++ scala/src/test/scala/KataSpec.scala
@@ -8,4 +8,26 @@ class DiamondSpec extends FlatSpec with Matchers {
 		result should be(Seq(Seq('A')))
   }
 
+	it should "work for B" in {
+		val diamond = new Diamond
+		val result = diamond('B')
+		result should be(Seq(
+			Seq(' ', 'A', ' '),
+			Seq('B', ' ', 'B'),
+			Seq(' ', 'A', ' ')
+		))
+	}
+
+	it should "work for C" in {
+		val diamond = new Diamond
+		val result = diamond('C')
+		result should be(Seq(
+			Seq(' ', ' ', 'A', ' ', ' '),
+			Seq(' ', 'B', ' ', 'B', ' '),
+			Seq('C', ' ', ' ', ' ', 'C'),
+			Seq(' ', 'B', ' ', 'B', ' '),
+			Seq(' ', ' ', 'A', ' ', ' '),
+		))
+	}
+
 }
