d302343ff08a7a27338f519536e9be34cf4842db
Jean Detoeuf <jean@upcoding.fr>
2019-12-02 23:06:52 +0100

    refactor

--- scala/src/main/scala/Diamond.scala
+++ scala/src/main/scala/Diamond.scala
@@ -2,31 +2,19 @@
 
 class Diamond {
 
-	def apply(letter: Char): Seq[Seq[Char]] = letter match {
-		// nombre de lignes : 2 n - 1
-		// nombres de colonnes : 2 n - 1
-		case 'A' => Seq(Seq('A'))
-		case 'B' => Seq(
-			diamondLine('B')('A'),
-			diamondLine('B')('B'),
-			diamondLine('B')('A')
-		)
-		case _ => Seq(
-			diamondLine('C')('A'),
-			diamondLine('C')('B'),
-			diamondLine('C')('C'),
-			diamondLine('C')('B'),
-			diamondLine('C')('A')
-		)
-	}
+	def apply(letter: Char): Seq[Seq[Char]] = diamondLetters(letter).map(diamondLine(letter))
+
+	private def diamondLetters(diamondLetter: Char): Seq[Char] = ('A' until diamondLetter) ++ ('A' to diamondLetter).reverse
 
 	private def diamondLine(diamondLetter: Char)(currentLetter: Char): Seq[Char] = {
-		line(2 * (diamondLetter - 'A') + 1, currentLetter, diamondLetter - currentLetter)
+		(0 until length(diamondLetter)).map(i =>
+			if (i == diamondLetter - currentLetter || i == length(diamondLetter) - (diamondLetter - currentLetter) - 1) currentLetter
+			else ' '
+		)
 	}
 
-	private def line(length: Int, letter: Char, position: Int) = {
-		Range(0, length).map(i => if (i == position || i == length - position - 1) letter else ' ')
+	private def length(diamondLetter: Char) = {
+		2 * (diamondLetter - 'A') + 1
 	}
 
-	def method(input: String) = Integer.valueOf(input)
 }
